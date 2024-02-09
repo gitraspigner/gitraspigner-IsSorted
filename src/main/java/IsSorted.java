bb8a9fdbebb557183e69e54815166f1821da70e9
Ravi Spigner <585pieces@protonmail.com>
2024-02-09 00:52:36 +0000

    completed Coding Challenge: Is Sorted

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class IsSorted {
     /**
      * Return true if arr is sorted, smallest to largest, false otherwise.
@@ -7,6 +11,19 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        List<Integer> sorted = new ArrayList<Integer>();
+        //copy.addAll(Arrays.asList(arr)); //couldn't get this to work
+        for (int i = 0; i < arr.length; i++) {
+            sorted.add(arr[i]);
+        }
+        Collections.sort(sorted);
+        //Collections.reverse(copy);  //now sorted least to greatest
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != sorted.get(i)) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
